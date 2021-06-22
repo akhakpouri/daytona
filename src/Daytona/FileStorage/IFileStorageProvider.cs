04511baefbeae0a0c4057d8cd1d60eeffcc63e78
Ali Khakpouri <ali.khakpouri@gmail.com>
2021-06-22 17:03:39 -0400

    Added the file storage, and unit test

---
+++ src/Daytona/FileStorage/IFileStorageProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Daytona.FileStorage
+{
+    public interface IFileStorageProvider
+    {
+        Task<Guid> Save(Stream fileStream);
+        Task<Stream> Read(Guid fileId);
+        Task Delete(Guid fileId);
+        Task<bool> IsExisting(Guid fileId);
+    }
+}
