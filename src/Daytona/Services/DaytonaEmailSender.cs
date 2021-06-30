e046ca53666c652018b5130510c6d1c3b875830c
Ali Khakpouri <ali.khakpouri@gmail.com>
2021-06-30 16:20:35 -0400

    Added the email sender, implemented a sendgrid test.

---
+++ src/Daytona/Services/DaytonaEmailSender.cs
@@ -0,0 +1,32 @@
+﻿using Daytona.Options;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace Daytona.Services
+{
+    public class DaytonaEmailSender : IDaytonaEmailSender
+    {
+        public AuthMessageSenderOptions Options { get; set; }
+
+        public DaytonaEmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
+        {
+            Options = optionsAccessor.Value;
+        }
+
+        public Task SendEmailAsync(string toEmailAddress, string subject, string message, string maskedName)
+        {
+            return Execute(Options.Key, Options.User, subject, message, toEmailAddress, maskedName);
+        }
+
+        private async Task Execute(string apiKey, string fromAddress, string subject, string message, string toEmailAddress, string maskedName)
+        {
+            var client = new SendGridClient(new SendGridClientOptions { ApiKey = apiKey, HttpErrorAsException = true });
+            var from = new EmailAddress(fromAddress, maskedName);
+            var to = new EmailAddress(toEmailAddress);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, string.Empty, message);
+            await client.SendEmailAsync(msg);
+        }
+    }
+}
