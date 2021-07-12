cb4b28ba684f7a24058852ee1e036fd6bc2f2db1
Ali Khakpouri <ali.khakpouri@gmail.com>
2021-07-12 15:13:08 -0400

    Added a friendly exception handling middleware extension to the library

---
+++ src/Daytona/Extensions/FriendlyExceptionsHandlingMiddlewareExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Daytona.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Daytona.Extensions
+{
+    public static class FriendlyExceptionsHandlingMiddlewareExtensions
+    {
+        public static IServiceCollection AddUseFriendlyExceptionsMiddleware(this IServiceCollection services) =>
+            services.AddScoped<FriendlyExceptionsHandlingMiddleware>();
+
+        public static IApplicationBuilder UseFriendlyExceptionsHandling(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<FriendlyExceptionsHandlingMiddleware>();
+        }
+    }
+}
