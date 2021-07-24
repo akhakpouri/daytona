aa7edb4c2fb23d2e260d67d24883413dc1e6402a
Ali Khakpouri <ali.khakpouri@gmail.com>
2021-07-24 12:21:58 -0400

    Added the constructor to the manager while injecting TUnitOfWork and IMapper

--- src/Daytona/Manager.cs
+++ src/Daytona/Manager.cs
@@ -1,4 +1,5 @@
-﻿using Daytona.Dto;
+﻿using AutoMapper;
+using Daytona.Dto;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -6,6 +7,15 @@ namespace Daytona
 {
     public abstract class Manager<TUnitOfWork, TDto> : IManager<TUnitOfWork, TDto> where TDto : BaseDto
     {
+        protected readonly TUnitOfWork UnitOfWork;
+        protected readonly IMapper Mapper;
+
+        public Manager(TUnitOfWork unitOfWork, IMapper mapper) 
+        {
+            UnitOfWork = unitOfWork;
+            Mapper = mapper;
+        }
+
         public abstract Task Delete(int id);
         public abstract Task<List<TDto>> GetAll();
         public abstract Task<TDto> GetById(int id);
